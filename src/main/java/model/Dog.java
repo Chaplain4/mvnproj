f77cab8468f8458cd5dd45dc53bf545db402fba2
Arthur Auskern <Chaplain04@gmail.com>
2023-07-05 20:49:26 +0300

    Dog class added

---
+++ src/main/java/model/Dog.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Dog {
+    private int id;
+    private String name;
+    private int age;
+
+}
